fca1aa3220f4a258985c6178cb2f164e1fb01b5d
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-08-04 16:50:17 +0300

    added some changes

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+home = lambda req : HttpResponse('<h1 >Home Page</h1><button >click here</button><input />')
+
+room = lambda req : HttpResponse('ROOM')
--- studypal/urls.py
+++ studypal/urls.py
@@ -1,14 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-home = lambda req : HttpResponse('<h1 style="color:red;">Home Page</h1><button rf="www.google.com">click here</button><input />')
-
-room = lambda req : HttpResponse('ROOM')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
+    path('', include('base.urls')),
+   
 ]
