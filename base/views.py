cfc27b31a155a1d1959c3480f48066593db24aa6
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-08-06 17:37:25 +0300

    added room form template

--- base/views.py
+++ base/views.py
@@ -14,3 +14,7 @@ def room(req, pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(req, 'base/room.html', context)
+
+def createRoom(req):
+    context = {}
+    return render(req, 'base/room_form.html', context )
